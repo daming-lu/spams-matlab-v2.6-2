37ee81799b55054a6ba7bf9844cbda424ce69434
daminglu <daminglu@yahoo.com>
2018-09-23 18:07:42 -0700

    gamma 4 is best result

--- test_release/daming_test_TrainDL.m
+++ test_release/daming_test_TrainDL.m
@@ -23,73 +23,73 @@ param.verbose=false;
 param.iter=1000;  % let us see what happens after 1000 iterations.
 
 %%%%%%%%%% FIRST EXPERIMENT %%%%%%%%%%%
-tic
-D = mexTrainDL(X,param);
-t=toc;
-fprintf('time of computation for Dictionary Learning: %f\n',t);
-
-fprintf('Evaluating cost function...\n');
-alpha=mexLasso(X,D,param);
-R=mean(0.5*sum((X-D*alpha).^2)+param.lambda*sum(abs(alpha)));
-% ImD=displayPatches(D);
-ImD = D;
-% subplot(1,3,1);
-
-%% Show first 20 2-digit images
-figure('Name','1st exp')
-for i=1:20
-%     fprintf('%d. Hello world!\n', i);
-    subplot(4,5,i)
-    row1matfull = full(reshape(alpha(i,:),[],56));
-    imshow(row1matfull);
-end
-
-% imagesc(ImD); colormap('gray');
-% fprintf('objective function: %f\n',R);
-% drawnow;
-
-fprintf('*********** SECOND EXPERIMENT ***********\n');
-%%%%%%%%%% SECOND EXPERIMENT %%%%%%%%%%%
-% Train on half of the training set, then retrain on the second part
-X1=X(:,1:floor(size(X,2)/2));
-X2=X(:,floor(size(X,2)/2):end);
-param.iter=500;
-tic
-[D model] = mexTrainDL(X1,param);
-t=toc;
-fprintf('time of computation for Dictionary Learning: %f\n',t);
-fprintf('Evaluating cost function...\n');
-alpha=mexLasso(X,D,param);
-
+% tic
+% D = mexTrainDL(X,param);
+% t=toc;
+% fprintf('time of computation for Dictionary Learning: %f\n',t);
+% 
+% fprintf('Evaluating cost function...\n');
+% alpha=mexLasso(X,D,param);
+% R=mean(0.5*sum((X-D*alpha).^2)+param.lambda*sum(abs(alpha)));
+% % ImD=displayPatches(D);
+% ImD = D;
+% % subplot(1,3,1);
+% 
+% %% Show first 20 2-digit images
+% figure('Name','1st exp')
+% for i=1:20
+% %     fprintf('%d. Hello world!\n', i);
+%     subplot(4,5,i)
+%     row1matfull = full(reshape(alpha(i,:),[],56));
+%     imshow(row1matfull);
+% end
+% 
+% % imagesc(ImD); colormap('gray');
+% % fprintf('objective function: %f\n',R);
+% % drawnow;
+% 
+% fprintf('*********** SECOND EXPERIMENT ***********\n');
+% %%%%%%%%%% SECOND EXPERIMENT %%%%%%%%%%%
+% % Train on half of the training set, then retrain on the second part
+% X1=X(:,1:floor(size(X,2)/2));
+% X2=X(:,floor(size(X,2)/2):end);
+% param.iter=500;
+% tic
+% [D model] = mexTrainDL(X1,param);
+% t=toc;
+% fprintf('time of computation for Dictionary Learning: %f\n',t);
+% fprintf('Evaluating cost function...\n');
+% alpha=mexLasso(X,D,param);
+% 
+% % R=mean(0.5*sum((X-D*alpha).^2)+param.lambda*sum(abs(alpha)));
+% % fprintf('objective function: %f\n',R);
+% tic
+% % Then reuse the learned model to retrain a few iterations more.
+% param2=param;
+% param2.D=D;
+% [D model] = mexTrainDL(X2,param2,model);
+% %[D] = mexTrainDL(X,param);
+% t=toc;
+% fprintf('time of computation for Dictionary Learning: %f\n',t);
+% fprintf('Evaluating cost function...\n');
+% alpha=mexLasso(X,D,param);
 % R=mean(0.5*sum((X-D*alpha).^2)+param.lambda*sum(abs(alpha)));
 % fprintf('objective function: %f\n',R);
-tic
-% Then reuse the learned model to retrain a few iterations more.
-param2=param;
-param2.D=D;
-[D model] = mexTrainDL(X2,param2,model);
-%[D] = mexTrainDL(X,param);
-t=toc;
-fprintf('time of computation for Dictionary Learning: %f\n',t);
-fprintf('Evaluating cost function...\n');
-alpha=mexLasso(X,D,param);
-R=mean(0.5*sum((X-D*alpha).^2)+param.lambda*sum(abs(alpha)));
-fprintf('objective function: %f\n',R);
-
-%% Show first 20 2-digit images
-figure('Name','2nd exp')
-for i=1:20
-%     fprintf('%d. Hello world!\n', i);
-    subplot(4,5,i)
-    row1matfull = full(reshape(alpha(i,:),[],56));
-    imshow(row1matfull);
-end
+% 
+% %% Show first 20 2-digit images
+% figure('Name','2nd exp')
+% for i=1:20
+% %     fprintf('%d. Hello world!\n', i);
+%     subplot(4,5,i)
+%     row1matfull = full(reshape(alpha(i,:),[],56));
+%     imshow(row1matfull);
+% end
 
 % let us add sparsity to the dictionary itself
 fprintf('*********** THIRD EXPERIMENT ***********\n');
 param.modeParam=0;
 param.iter=1000;
-param.gamma1=0.15;
+param.gamma1=5;
 param.modeD=1;
 
 tic
@@ -131,7 +131,7 @@ fprintf('objective function: %f\n',R);
 tic
 
 %% Show first 20 2-digit images
-figure('Name','3rd exp')
+figure('Name','2-D MNIST dictionary atoms')
 for i=1:20
 %     fprintf('%d. Hello world!\n', i);
     subplot(4,5,i)
